786dc9d02fa7501d3a00085964901ef0dc07775b
DaffodilGirl <gillhd22@gmail.com>
2020-05-29 20:32:30 +0100

    First commit

---
+++ Section1Ex2/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Section1Ex2
+{
+    public class Post
+    {
+        private string _title;
+        private string _description;
+        private readonly DateTime _timestamp;
+        private int _upVote = 0;
+        private int _downVote = 0;
+
+        public int UpVote { get { return _upVote; } }
+
+        public int DownVote { get { return _downVote; } }
+
+
+        public Post(string title, string desc)
+        {
+            this._timestamp = DateTime.Now;
+            this._title = title;
+            this._description = desc;
+        }
+
+
+        public void ShowPost()
+        {
+            int divSize = Math.Max(this._title.Length + 7, this._description.Length + 9);
+            string div1 = new String('=', divSize);
+            string div2 = new String('-', divSize);
+            Console.WriteLine("\n{0}", div1);
+            Console.WriteLine("Title: {0}\n[Timestamp: {1}]", this._title, this._timestamp);
+            Console.WriteLine("{0}", div2);
+            Console.WriteLine("Comment: {0}", this._description);
+            Console.WriteLine("{0}", div1);
+        }
+
+
+        public void Vote(string vote)
+        {
+            if (vote.ToLower() == "up") _upVote += 1;
+            else if (vote.ToLower() == "down") _downVote += 1;
+            else throw new Exception("Invalid Vote");
+        }
+
+    }
+
+}
---
+++ Section1Ex2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section1Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Dictionary<string, string> {
+                        { "up", "Like Post" },
+                        { "down", "DisLike Post" },
+                        { "show", "Show Post Likes" },
+                        { "quit", "Quit" }
+            };
+
+            Console.Write(">> Please input post title: ");
+            var title = Console.ReadLine();
+            Console.Write(">> Please input post comment: ");
+            var desc = Console.ReadLine();
+            
+            var post = new Post(title, desc);
+            post.ShowPost();
+
+            bool doRun = true;
+            while (doRun)
+            {
+                try
+                {
+                    var action = ListOptions(options);
+                    if (action == "up" || action == "down") post.Vote(action);
+                    else if (action == "show")
+                    {
+                        Console.WriteLine("\n--> Post Likes: {0}; Dislikes: {1}", post.UpVote, post.DownVote);
+                    }
+                    else if (action == "quit") doRun = false;
+                    else Console.WriteLine(">> Unknown option - try again");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+
+        static string ListOptions(Dictionary<string,string> optlist)
+        {
+            var options = new List<string>();
+            foreach (var d in optlist) options.Add(d.Key);
+
+            Console.WriteLine("\n>> Please select option:");
+            int idx = 0;
+            foreach (var opt in optlist)
+            {
+                Console.WriteLine("{0,3}. {1}", idx+1, optlist[options[idx++]]);
+            }
+            return options[InputInteger("Input option number", 1, optlist.Count)-1];
+        }
+
+
+        public static int InputInteger(string text, int? min, int? max)
+        {
+            bool ok = false;
+            int num = 0;
+            string range;
+
+            // Number range display
+            if (min.HasValue && max.HasValue) { range = " (" + min + "-" + max + ")"; }
+            else if (max.HasValue) { range = " (<=" + max + ")"; }
+            else if (min.HasValue) { range = " (>=" + min + ")"; }
+            else range = "";
+
+            if (!min.HasValue) { min = int.MinValue; }
+            if (!max.HasValue) { max = int.MaxValue; }
+
+            if (string.IsNullOrEmpty(text)) { text = "Input an integer"; }
+            while (!ok)
+            {
+                Console.Write("{0}{1}: ", text, range);
+                string numstr = Console.ReadLine();
+                ok = int.TryParse(numstr, out num);
+                ok = (ok && num >= min && num <= max);
+                if (!ok) Console.WriteLine("Invalid input - please try again");
+            }
+            return num;
+        }
+    }
+
+}
